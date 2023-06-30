d66b3e7433d55b20f9394720e91ba99acd987480
Feplyss <fellipe.bomfim10@gmail.com>
2023-06-30 02:44:31 -0300

    fix: moving http exception from controller to response error manager

--- src/main/java/com/senac/projetoIntegrador/atividade/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/senac/projetoIntegrador/atividade/exceptions/ResourceExceptionHandler.java
@@ -2,8 +2,11 @@ package com.senac.projetoIntegrador.atividade.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.senac.projetoIntegrador.atividade.response.ErrorResponse;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -15,5 +18,11 @@ public class ResourceExceptionHandler {
         StandardError errors = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Validation error", "Usuário não encontrado.", request.getRequestURI());
         
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("Error parsing JSON", e.getMessage(), null));
     }
 }
